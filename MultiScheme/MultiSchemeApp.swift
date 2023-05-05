4b0f5bbda16e14a47bf304e8c3b76ed30699e384
motoshi-suzuki <motoshi.suzukiii@gmail.com>
2023-05-05 10:55:11 +0900

    initial commit

---
+++ MultiScheme/MultiSchemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiSchemeApp.swift
+//  MultiScheme
+//
+//  Created by Motoshi Suzuki on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct MultiSchemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
